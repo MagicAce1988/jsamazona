fd086819ff693093b2c9d45c1d12248ee2e46f94
Talmacel Marian-Silviu <magicace1988@gmail.com>
2021-02-16 18:02:23 +0000

    sign in screen ui done

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,7 @@ import CartScreen from './screens/CartScreen';
 import Error404Screen from './screens/Error404Screen';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
+import SignInScreen from './screens/SignInScreen';
 import { parseRequestUrl } from './utils';
 
 const routes = {
@@ -9,6 +10,7 @@ const routes = {
   '/product/:id': ProductScreen,
   '/cart': CartScreen,
   '/cart/:id': CartScreen,
+  '/signin': SignInScreen,
 };
 
 const router = async () => {
