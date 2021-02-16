cb1613d75ac0556edffba5920250cbd21d5350e5
Talmacel Marian-Silviu <magicace1988@gmail.com>
2021-02-16 20:18:43 +0000

    finished sign in screen action

--- frontend/src/localStorage.js
+++ frontend/src/localStorage.js
@@ -8,3 +8,27 @@ export const getCartItems = () => {
 export const setCartItems = (cartItems) => {
   localStorage.setItem('cartItems', JSON.stringify(cartItems));
 };
+
+export const setUserInfo = ({
+  _id = '',
+  name = '',
+  email = '',
+  token = '',
+  isAdmin = '',
+}) => {
+  localStorage.setItem(
+    'userInfo',
+    JSON.stringify({ _id, name, email, token, isAdmin })
+  );
+};
+
+export const getUserInfo = () =>
+  localStorage.getItem('userInfo')
+    ? JSON.parse(localStorage.getItem('userInfo'))
+    : {
+        _id: '',
+        name: '',
+        email: '',
+        token: '',
+        isAdmin: '',
+      };
--- frontend/src/screens/SignInScreen.js
+++ frontend/src/screens/SignInScreen.js
@@ -1,10 +1,32 @@
+/* eslint-disable no-alert */
+
+import { signIn } from '../api';
+import { getUserInfo, setUserInfo } from '../localStorage';
+
 const SignInScreen = {
-  after_render: () => {},
+  after_render: () => {
+    const signInForm = document.getElementById('signin-form');
+    signInForm.addEventListener('submit', async (e) => {
+      e.preventDefault();
+      const data = await signIn({
+        email: document.getElementById('email').value,
+        password: document.getElementById('password').value,
+      });
+      if (data.error) {
+        alert(data.error);
+      } else {
+        setUserInfo(data);
+        document.location.hash = '/';
+      }
+    });
+  },
   render: () => {
-    const some = 8;
+    if (getUserInfo().name) {
+      document.location.hash = '/';
+    }
     return `
     <div class="form-container">
-        <form id="signin form">
+        <form id="signin-form">
             <ul class="form-items">
                 <li>
                     <h1>Sign-In</h1>
