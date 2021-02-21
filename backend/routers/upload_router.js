10f2d4c0d64b8760e05588b1541cc4f041beb022
Talmacel Marian-Silviu <magicace1988@gmail.com>
2021-02-21 22:05:24 +0000

    changes done

--- backend/routers/upload_router.js
+++ backend/routers/upload_router.js
@@ -1,23 +1,41 @@
 /* eslint-disable camelcase */
 import express from 'express';
 import multer from 'multer';
+import AWS from 'aws-sdk';
+import { v4 as uuidv4 } from 'uuid';
 import { isAdmin, isAuth } from '../utils';
+import config from '../config';
 
-const storage = multer.diskStorage({
+const s3 = new AWS.S3({
+  accessKeyId: config.AWS_ID,
+  secretAccessKey: config.AWS_SECRET,
+});
+
+const storage = multer.memoryStorage({
   destination(req, file, cb) {
-    cb(null, 'uploads/');
-  },
-  filename(req, file, cb) {
-    cb(null, `${Date.now()}.jpg`);
+    cb(null, '');
   },
 });
 
-const upload = multer({ storage });
+const upload = multer({ storage }).single('image');
 
 const upload_router = express.Router();
 
-upload_router.post('/', isAuth, isAdmin, upload.single('image'), (req, res) => {
-  res.status(201).send({ image: `${req.file.path}` });
+upload_router.post('/', isAuth, isAdmin, upload, (req, res) => {
+  const myFile = req.file.originalname.split('.');
+  const fileType = myFile[myFile.length - 1];
+
+  const params = {
+    Bucket: config.AWS_BUCKET_NAME,
+    Key: `${uuidv4()}.${fileType}`,
+    Body: req.file.buffer,
+  };
+  s3.upload(params, (error, data) => {
+    if (error) {
+      res.status(500).send({ message: error });
+    }
+    res.status(201).send({ image: data.Location });
+  });
 });
 
 export default upload_router;
--- backend/server.js
+++ backend/server.js
@@ -38,7 +38,6 @@ app.use('/api/orders', order_router);
 app.get('/api/paypal/clientId', (req, res) => {
   res.send({ clientId: config.PAYPAL_CLIENT_ID });
 });
-app.use('/uploads', express.static(path.join(__dirname, '/../uploads')));
 app.use(express.static(path.join(__dirname, '/../frontend')));
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '/../frontend/index.html'));
